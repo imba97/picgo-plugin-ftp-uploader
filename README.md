0080a1c53e81baaa56a3a290b4115539543136c8
imba97 <mail@imba97.cn>
2021-01-05 21:49:45 +0800

    Hello PicGo

---
+++ README.md
@@ -0,0 +1,45 @@
+## picgo-plugin-ftp-uploader
+
+PicGo FTP 上传插件
+
+## 配置
+
+名称 | 介绍 | 配置示例
+-|-|-
+域名地址 | 图片网站的域名 | https://imba97.cn
+网站路径 | 图片的路径 | /uploads/{year}/{month}/{fullName}
+文件路径 | 文件在FTP服务器的真实路径 | /blog/uploads/{year}/{month}/{fullName}
+FTP地址 | 略 | 233.233.233.233
+端口 | 略 | 21
+用户名 | 略 | imba97
+密码 | 略 | imba97
+
+最终返回的地址是 **域名地址** + **网站路径**
+
+## 路径 Format
+
+路径配置可使用以下参数，使用示例：`/{year}/{month}/{fullName}`，输出示例：`/2020/01/imba97.png`
+
+名称 | 介绍 | 输出示例
+-|-|-
+year | 当前年份 | 2021
+month | 当前月份 | 01
+fullName | 图片全名 | imba97.png
+fileName | 图片名称 | imba97
+hash16 | 图片 MD5 16位 | 68559cae1081d683
+hash32 | 图片 MD5 32位 | 68559cae1081d6836e09b043aa0b3af1
+ext | 图片后缀名 | png
+
+**注意**：除了`fullName`，其他都需要自行添加后缀名
+
+## 路径配置示例
+
+**网站路径**和**文件路径**的配置示例
+
+比如我服务器有这样一个路径：`/www/wwwroot/blog/uploads/`，图片在里面
+
+我的网站根目录是`/www/wwwroot/blog/`，而FTP根目录是`/www/wwwroot/`
+
+那么我可以把**网站路径**设置为`/uploads/{year}/{month}/{fullName}`
+
+**文件路径**设置为`/blog/uploads/{year}/{month}/{fullName}`
---
+++ src/index.ts
@@ -0,0 +1,243 @@
+import picgo from 'picgo'
+import ftpClient from 'ftp'
+import { IImgInfo } from 'picgo/dist/src/types'
+
+import crypto from 'crypto'
+
+const ftp = new ftpClient()
+
+interface IFtpLoaderUserConfig {
+  url: string
+  path: string
+  uploadPath: string
+  host: string
+  port: number
+  username?: string
+  password?: string
+}
+
+interface IFtpLoaderPathInfo {
+  path: string
+  uploadPath: string
+}
+
+export = (ctx: picgo) => {
+
+  const config = (ctx: picgo) => {
+    let userConfig = ctx.getConfig<IFtpLoaderUserConfig>('picBed.ftp-uploader')
+    if (!userConfig) {
+      userConfig = {
+        url: '',
+        path: '',
+        uploadPath: '/uploads/{year}/{month}',
+        host: '',
+        port: 21
+      }
+    }
+    return [
+      {
+        name: 'url',
+        type: 'input',
+        default: userConfig.url,
+        required: true,
+        message: 'Url by ftp host.',
+        alias: '域名地址'
+      },
+      {
+        name: 'path',
+        type: 'input',
+        default: userConfig.path,
+        required: true,
+        message: '/uploads/{year}/{month}/{fullName}',
+        alias: '网站路径'
+      },
+      {
+        name: 'uploadPath',
+        type: 'input',
+        default: userConfig.uploadPath,
+        required: true,
+        message: '/blog/uploads/{year}/{month}/{fullName}',
+        alias: '文件路径'
+      },
+      {
+        name: 'host',
+        type: 'input',
+        default: userConfig.host,
+        required: true,
+        message: 'Host by ftp.',
+        alias: 'FTP地址'
+      },
+      {
+        name: 'port',
+        type: 'input',
+        default: userConfig.port,
+        required: true,
+        message: 'Port by ftp.',
+        alias: '端口'
+      },
+      {
+        name: 'username',
+        type: 'input',
+        default: userConfig.username,
+        required: false,
+        message: 'Username by ftp.',
+        alias: '用户名（可选）'
+      },
+      {
+        name: 'password',
+        type: 'input',
+        default: userConfig.password,
+        required: false,
+        message: 'Password by ftp.',
+        alias: '密码（可选）'
+      }
+    ]
+  }
+
+  const handle = async (ctx: picgo) => {
+
+    let userConfig: IFtpLoaderUserConfig = ctx.getConfig('picBed.ftp-uploader')
+    if (!userConfig) {
+      throw new Error("Can't find uploader config")
+    }
+
+    ftp.connect({
+      host: userConfig.host,
+      prot: userConfig.port,
+      user: userConfig.username,
+      password: userConfig.password
+    })
+
+    await ftpOnReady()
+
+    let input = ctx.input
+    let output = ctx.output
+    let filesCount = input.length
+
+    for (let i in input) {
+      let localPath = input[i]
+
+      await upload(output[i], localPath)
+      .then(path => {
+
+        let imgUrl = `${/\/$/.test(userConfig.url) ? userConfig.url.substr(0, userConfig.url.length) : userConfig.url}${path}`
+
+        delete output[i].buffer
+
+        output[i].url = imgUrl
+        output[i].imgUrl = imgUrl
+
+        filesCount--
+        if(filesCount <= 0) ftp.end()
+      })
+      .catch(err => {
+        ctx.log.error(`err: ${err}`)
+      })
+
+    }
+
+    return ctx
+  }
+  
+  const ftpOnReady = (): Promise<string> => {
+    return new Promise((resolve, reject) => {
+      ftp.on('ready', () => {
+        resolve('OK')
+      })
+    })
+  }
+
+  const upload = (output: IImgInfo, localPath: string) : Promise<string> => {
+
+    let userConfig: IFtpLoaderUserConfig = ctx.getConfig('picBed.ftp-uploader')
+
+    // 获取日期
+    let date = new Date()
+
+    // 格式化数据
+    let formatData = {
+
+      // 路径
+      year: `${date.getFullYear()}`,
+      month: date.getMonth() < 9 ? `0${date.getMonth() + 1}` : `${date.getMonth() + 1}`,
+
+      // 文件名
+      fullName: output.fileName,
+      fileName: output.fileName.replace(output.extname, ''),
+      hash16: crypto.createHash('md5').update(output.base64Image ? output.base64Image : output.buffer.toString()).digest('hex').substr(0, 16),
+      hash32: crypto.createHash('md5').update(output.base64Image ? output.base64Image : output.buffer.toString()).digest('hex'),
+
+      // 后缀名
+      ext: output.extname.replace('.', ''),
+    }
+    // 未格式化路径
+    let pathInfo: IFtpLoaderPathInfo = {
+      path: userConfig.path,
+      uploadPath: userConfig.uploadPath
+    }
+    // 替换后的路径
+    let formatPath: IFtpLoaderPathInfo = {
+      path: '',
+      uploadPath: ''
+    }
+
+    for(let key in pathInfo) {
+      // 匹配 {} 内容
+      let out = 0
+      let reg = /(?:{(\w+)})/g
+      formatPath[key] = pathInfo[key]
+      let result: RegExpExecArray
+      while(result = reg.exec(pathInfo[key])) {
+
+        // 替换文本
+        formatPath[key] = formatPath[key].replace(result[0], formatData[result[1]])
+
+        // 避免死循环 一般没啥问题
+        out++
+        if(out > 100) break
+      }
+
+    }
+
+    // 自动创建文件夹 然后执行上传
+    return new Promise((resolve, reject) => {
+
+      let path = formatPath.uploadPath.substr(0, formatPath.uploadPath.lastIndexOf('/') + 1)
+
+      ftp.get(path, (err) => {
+        if(err) {
+          ftp.mkdir(path, true, (err) => {
+            if(err) reject(err)
+            resolve(doUpload(localPath, formatPath))
+          })
+        } else {
+          resolve(doUpload(localPath, formatPath))
+        }
+      })
+    })
+    
+  }
+
+  const doUpload = (localPath: string, formatPath: IFtpLoaderPathInfo) : Promise<string> => {
+    return new Promise(function (resolve, reject) {
+      ftp.put(localPath, formatPath.uploadPath, function(err) {
+        if (err) reject(err)
+        resolve(formatPath.path)
+      })
+    })
+    
+  }
+
+  const register = () => {
+    ctx.helper.uploader.register('ftp-uploader', {
+      handle,
+      config,
+      name: 'FTP上传'
+    })
+  }
+
+  return {
+    uploader: 'ftp-uploader',
+    register
+  }
+}
